eeb055b4bc7a90a2ff2a3cd4ae0ff8b6acba6a67
thang111 <thangnv111@fpt.com>
2025-11-17 01:04:08 +0700

    fix save radio button zoom video

--- ReviewMovie/FormMain.cs
+++ ReviewMovie/FormMain.cs
@@ -545,8 +545,8 @@ namespace ReviewMovie
                 nScaleAudioRangeStart.Value = stdefault ? (decimal)1.1 : valEffectSettup.SscaleAudioRangeStart;
                 nScaleAudioRangeEnd.Value = stdefault ? (decimal)1.2 : valEffectSettup.SscaleAudioRangeEnd;
 
-                CkZoom.Checked = stdefault ? true
-                                            : valEffectSettup.SckZoom ? true : true;
+                CkZoom.Checked = stdefault ? false
+                                            : valEffectSettup.SckZoom ? true : false;
                 ckRotate.Checked = stdefault ? false
                                             : valEffectSettup.SckRotate ? true : false;
                 ckHflip.Checked = stdefault ? false
@@ -2668,6 +2668,7 @@ namespace ReviewMovie
 
                 _infoProject = _projectService.GetProjectDetail(newProject.ID, projectPath);
                 DefaultProjectData();
+                _statusZoom = true; // Khai báo cờ check
                 MessageBox.Show("Tạo Project Thành Công !");
             }
             catch (Exception ex)
