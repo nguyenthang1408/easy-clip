d28c21415e54214202f65bc135434e2623ebe122
toanpm <phammanhtoan.hd91@gmail.com>
2026-02-10 01:29:32 +0700

    add field in response

--- LibCommon/Lib/Model/Package/GetVersionResponse.cs
+++ LibCommon/Lib/Model/Package/GetVersionResponse.cs
@@ -11,6 +11,9 @@ namespace LibCommon.Lib.Model.Package
         [JsonProperty("isSuccess")]
         public bool IsSuccess { get; set; }
 
+        [JsonProperty("message")]
+        public string Message { get; set; }
+
         [JsonProperty("version")]
         public string Version { get; set; }
 
